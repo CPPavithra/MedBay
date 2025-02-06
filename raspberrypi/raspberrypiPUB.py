3987a3d17fbf36b1b66230a5676b08894117a95e
Ayush Daga <dagaayush1205@gmail.com>
2025-02-06 12:30:20 +0530

    code for raspberry pi

---
+++ raspberrypi/raspberrypiPUB.py
@@ -0,0 +1,13 @@
+import socket
+s = socket.socket()
+print("Socket created successfully")
+port = 8000
+s.bind(('127.0.0.1', port))
+print("socket has been binded to port 8000")
+s.listen(5)
+while True:
+    c, addr = s.accept()
+    print("got connection from ", addr)
+    c.send("ALERT".encode())
+    break
+
