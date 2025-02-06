3b3ff0e7b33c9661ee9bdd18dacaaba6a19ebb88
Ayush Daga <dagaayush1205@gmail.com>
2025-02-06 22:30:12 +0530

    changed IP for pi

--- raspberrypi/raspberrypiPUB.py
+++ raspberrypi/raspberrypiPUB.py
@@ -2,12 +2,13 @@ import socket
 s = socket.socket()
 print("Socket created successfully")
 port = 8000
-s.bind(('127.0.0.1', port))
+s.bind(('192.168.207.245', port))
 print("socket has been binded to port 8000")
 s.listen(5)
 while True:
     c, addr = s.accept()
     print("got connection from ", addr)
-    c.send("ALERT".encode())
+    c.send("hehe".encode())
     break
+c.close()
 
