937cbffa2d4a794e10a02b3dbe513761223fd866
Ayush Daga <dagaayush1205@gmail.com>
2025-02-06 12:51:53 +0530

    updated laptop code

--- raspberrypi/laptop.py
+++ raspberrypi/laptop.py
@@ -2,7 +2,5 @@ import socket
 s = socket.socket()
 port = 8000
 s.connect(('127.0.0.1', port))
-
-while True:
-    print(s.recv(1024).decode())
-    s.close()
+print(s.recv(1024).decode())
+s.close()
