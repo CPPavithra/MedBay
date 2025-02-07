5be22e8f89b8dde904808ebb402a8d4fbf61306f
Ayush Daga <dagaayush1205@gmail.com>
2025-02-07 19:13:07 +0530

    changed names and set ip of pi

--- raspberrypi/laptop.py
+++ raspberrypi/laptop.py
@@ -1,14 +1,6 @@
-import socket
+import socket 
 s = socket.socket()
-print("Socket created successfully")
 port = 8000
-s.bind(('192.168.207.151', port))
-print("socket has been binded to port 8000")
-s.listen(5)
-while True:
-    c, addr = s.accept()
-    print("got connection from ", addr)
-    c.send("hehe".encode())
-    break
-c.close()
-
+s.connect(('192.168.207.102', port))
+print(s.recv(1024).decode())
+s.close()
--- raspberrypi/raspberrypi.py
+++ raspberrypi/raspberrypi.py
@@ -1,6 +1,14 @@
-import socket 
+import socket
 s = socket.socket()
+print("Socket created successfully")
 port = 8000
-s.connect(('192.168.207.151', port))
-print(s.recv(1024).decode())
-s.close()
+s.bind(('192.168.207.102', port))
+print("socket has been binded to port 8000")
+s.listen(5)
+while True:
+    c, addr = s.accept()
+    print("got connection from ", addr)
+    c.send("hehe".encode())
+    break
+c.close()
+
