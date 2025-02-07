670138af8fadd66e15b93c1c2edf2864d95e79aa
Ayush Daga <dagaayush1205@gmail.com>
2025-02-08 01:50:50 +0530

    testing force sensor with pi

--- raspberrypi/laptop.py
+++ raspberrypi/laptop.py
@@ -2,5 +2,5 @@ import socket
 s = socket.socket()
 port = 8000
 s.connect(('192.168.207.102', port))
-print(s.recv(1024).decode())
-s.close()
+while True:
+    print(s.recv(1024).decode())
--- raspberrypi/raspberrypi.py
+++ raspberrypi/raspberrypi.py
@@ -1,14 +1,42 @@
 import socket
-s = socket.socket()
-print("Socket created successfully")
-port = 8000
-s.bind(('192.168.207.102', port))
-print("socket has been binded to port 8000")
-s.listen(5)
-while True:
-    c, addr = s.accept()
+import serial
+sensor = serial.Serial(port = '/dev/serial/by-id/usb-1a86_USB_Serial-if00-port0', baudrate = 9600)
+
+def crash_detection():
+    data = sensor.readline()
+    print(data)
+    if data == b'1\r\n':
+        print("ALERT: CRASH DETECTED")
+        return 1
+    else: return 0
+
+def node_init():
+    node = socket.socket()
+    print("Socket created successfully")
+    port = 8000
+    node.bind(('192.168.207.102', port))
+    print("socket has been binded to port 8000")
+    node.listen(5)
+    c, addr = node.accept()
     print("got connection from ", addr)
-    c.send("hehe".encode())
-    break
-c.close()
+    return c
+
+
+def send_alert(c):
+    while True:
+        try:
+            c.send("ALERT: EMERGENCY".encode())
+            c.send("VEHICLE 1 in distress".encode())
+        except BrokenPipeError:
+            print("Disconnected")
+        break
+
+def main():
+    server = node_init()
+    while True:
+        if crash_detection() == 1:
+            send_alert(server)
+    
+if __name__=="__main__":
+    main()
 
