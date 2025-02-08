8893de66b47f77a5867790c12b3db5b21aa3c223
Ayush Daga <dagaayush1205@gmail.com>
2025-02-08 09:05:55 +0530

    function for detection_laptop side

--- raspberrypi/laptop.py
+++ raspberrypi/laptop.py
@@ -2,5 +2,16 @@ import socket
 s = socket.socket()
 port = 8000
 s.connect(('192.168.207.102', port))
-while True:
-    print(s.recv(1024).decode())
+def detect_accident():
+    while True:
+        status = s.recv(1024).decode()
+        print(status)
+        if status == 'ALERT: EMERGENCY':
+            return 1
+def main():
+    check = detect_accident()
+    if check == 1:
+        print("ACCIDENT OCCURED")
+if __name__ == "__main__":
+    main()
+
