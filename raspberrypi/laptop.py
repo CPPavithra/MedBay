a61eb85316db11d61c0c35ac76662589c94d1f0f
Ayush Daga <dagaayush1205@gmail.com>
2025-02-08 09:19:04 +0530

    intialization for laptop

--- raspberrypi/laptop.py
+++ raspberrypi/laptop.py
@@ -1,15 +1,18 @@
 import socket 
-s = socket.socket()
-port = 8000
-s.connect(('192.168.207.102', port))
-def detect_accident():
+def initialize_laptop():
+    s = socket.socket()
+    port = 8000
+    s.connect(('192.168.207.102', port))
+    return s
+def detect_accident(s):
     while True:
         status = s.recv(1024).decode()
         print(status)
         if status == 'ALERT: EMERGENCY':
             return 1
 def main():
-    check = detect_accident()
+    s = initialize_laptop()
+    check = detect_accident(s)
     if check == 1:
         print("ACCIDENT OCCURED")
 if __name__ == "__main__":
